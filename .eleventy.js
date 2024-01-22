e1d9c422f80ba157ab3d36934b6f33520c30de97
nscior <sciorti3@msu.edu>
2024-01-22 18:17:01 -0500

    localhost images work

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,6 @@
 module.exports = function(eleventyConfig) {
-    module.exports = function(eleventyConfig) {
-        // Find and copy any `jpg` files, maintaining directory structure.
-        eleventyConfig.addPassthroughCopy("img");
-        eleventyConfig.addPassthroughCopy("bundle.css");
-        eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
-      };
-  };
+    // Find and copy any `jpg` files, maintaining directory structure.
+    eleventyConfig.addPassthroughCopy("img");
+    eleventyConfig.addPassthroughCopy("bundle.css");
+    eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
+};

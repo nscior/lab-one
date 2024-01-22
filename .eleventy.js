e0baf3b087a956cd890de8e44ffc8140c84ca8ba
nscior <sciorti3@msu.edu>
2024-01-22 18:08:42 -0500

    guh

--- .eleventy.js
+++ .eleventy.js
@@ -3,5 +3,6 @@ module.exports = function(eleventyConfig) {
         // Find and copy any `jpg` files, maintaining directory structure.
         eleventyConfig.addPassthroughCopy("img");
         eleventyConfig.addPassthroughCopy("bundle.css");
+        eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
       };
   };

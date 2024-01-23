43019c510e0b3417ebb3e72abcdfd36ed88bdf5f
Nickolas Sciortino <sciorti3@msu.edu>
2024-01-22 22:49:03 -0500

    no way

--- .eleventy.js
+++ .eleventy.js
@@ -2,5 +2,6 @@ module.exports = function(eleventyConfig) {
     // Find and copy any `jpg` files, maintaining directory structure.
     eleventyConfig.addPassthroughCopy("img");
     eleventyConfig.addPassthroughCopy("bundle.css");
+    eleventyConfig.addPassthroughCopy("*.html");
     eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
 };
